f3a25835592f17d15f053ab9ee57cae656669da8
Github leonmezu1 <leo.mez@outlook.com>
2021-01-04 09:15:23 -0500

    Anadir elementos a el formulario

--- src/components/Formulario.js
+++ src/components/Formulario.js
@@ -1,10 +1,39 @@
+/* eslint-disable jsx-a11y/label-has-associated-control */
+
 const Formulario = () => (
   <div className="bg-info">
     <div className="container">
       <div className="row">
         <div className="col card text-white bg-transparent mb-5 pt-5 pb-2">
           <fieldset>
-            <legend className="text-center">Buscador de letras y canciones</legend>
+            <legend className="text-center">
+              Buscador de letras y canciones
+            </legend>
+
+            <div className="row">
+              <div className="col-md-6">
+                <div className="form-group">
+                  <label htmlFor="artista">Artista</label>
+                  <input
+                    type="text"
+                    name="artista"
+                    className="form-control"
+                    placeholder="Nombre del artista"
+                  />
+                </div>
+              </div>
+              <div className="col-md-6">
+                <div className="form-group">
+                  <label htmlFor="cancion">Cancion</label>
+                  <input
+                    type="text"
+                    name="cancion"
+                    className="form-control"
+                    placeholder="Nombre de la cancion"
+                  />
+                </div>
+              </div>
+            </div>
           </fieldset>
         </div>
       </div>
